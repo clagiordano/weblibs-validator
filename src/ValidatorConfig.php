d1666f91fe36cecf692524b2aa71b4f4de36dd7a
Claudio Giordano <claudio.giordano@autistici.org>
2016-09-10 22:37:04 +0200

    Update composer dependencies and validator configuration

--- src/ValidatorConfig.php
+++ src/ValidatorConfig.php
@@ -7,5 +7,14 @@ namespace clagiordano\weblibs\validator;
  */
 class ValidatorConfig
 {
-
+    public static $allowedValidator = [
+        'required' => 'Required',
+        'minlenght' => 'Minlenght',
+        'maxlenght' => 'Maxlenght',
+        'email' => 'Email',
+        'alnum' => 'Alphanumeric',
+        'match' => 'Match',
+        'unique' => 'Unique',
+        'regexp' => 'Regexp',
+    ];
 }
