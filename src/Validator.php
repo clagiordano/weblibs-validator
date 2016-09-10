6f518c67accc1bf24735e9a20a954c3378502acb
Claudio Giordano <claudio.giordano@autistici.org>
2016-09-10 22:10:13 +0200

    Added first properties and setter for validator

--- src/Validator.php
+++ src/Validator.php
@@ -2,10 +2,32 @@
 
 namespace clagiordano\weblibs\validator;
 
+use ErrorHandler;
+
 /**
  *
  */
 class Validator
 {
+    /** @var ErrorHandler $errorHandler */
+    protected $errorHandler = null;
+    /** @var array $validatorRules */
+    protected $validatorRules = [];
+    /** @var array $validatorMessages */
+    protected $validatorMessages = [];
 
+    public function __construct(ErrorHandler $errorHandler)
+    {
+        $this->errorHandler = $errorHandler;
+    }
+
+    public function setRules(array $validatorRules = [])
+    {
+        $this->validatorRules = $validatorRules;
+    }
+
+    public function setMessages(array $validatorMessages = [])
+    {
+        $this->validatorMessages = $validatorMessages;
+    }
 }
